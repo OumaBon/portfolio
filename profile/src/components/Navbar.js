189d922104e1c9fb36ab25b03318761375e1a33f
McMugah <blainelium@gmail.com>
2024-07-08 14:39:54 +0300

    updates

---
+++ profile/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function Navbar(){
+    return (
+        <header className='container'>
+            <h1>BO</h1>
+
+            <nav className='navbar'>
+                <li>Projects</li>
+                <li>Experience</li>
+                <li>About Me</li>
+                <li>Contact</li>
+            </nav>
+            <button className='btn'>Hire Me</button>
+        </header>
+
+    )
+}
