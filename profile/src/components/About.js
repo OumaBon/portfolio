ab0c9cf593f9d7bbb4a83d0d5da36d28436d1e13
OumaBon <mugahboniface@gmail.com>
2024-07-09 13:38:25 +0300

    about page

---
+++ profile/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <P>the most important thing about the crime</P>
+        <p>Welcome again in the world of sin</p>
+    </div>
+  )
+}
+
+export default About
